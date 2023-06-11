ad72a47147b0f95d46d3ba68dba8882dec4e8785
Anitamaurya1211 <anita1211997@gmail.com>
2023-06-11 20:48:23 +0530

    backenddata

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json())
+ const cors = require('cors'); // Import the cors middleware
+ app.use(cors()); // Enable CORS for all routes
+
+
 
 app.get('/mywork/:id',(req,res)=>{
-    connection.query('SELECT * FROM mywork WHERE id=?',[req.params.id],(err,rows)=>{
+    connection.query('SELECT * FROM tasks WHERE id=?',[req.params.id],(err,rows)=>{
         if(err){
             console.log(err)
         }else {
@@ -15,9 +19,20 @@ app.get('/mywork/:id',(req,res)=>{
         }
     })
 })
+
+app.get('/mywork',(req,res)=>{
+    connection.query('SELECT * FROM tasks;',(err,rows)=>{
+        if(err){
+            console.log(err)
+        }else {
+            res.send(rows)
+
+        }
+    })
+}) 
 
 app.delete('/mywork/:id',(req,res)=>{
-    connection.query('DELETE FROM mywork WHERE id=?',[req.params.id],(err,rows)=>{
+    connection.query('DELETE FROM tasks WHERE id=?',[req.params.id],(err,rows)=>{
         if(err){
             console.log(err)
         }else {
@@ -26,11 +41,12 @@ app.delete('/mywork/:id',(req,res)=>{
         }
     })
 })
+
 
 app.post('/mywork/',(req,res)=>{
      var mywork = req.body
      var myworkData =[mywork.title,mywork.task]
-    connection.query('INSERT INTO mywork(title,task) values(?)',[myworkData],(err,rows)=>{
+    connection.query('INSERT INTO tasks(title,task) values(?)',[myworkData],(err,rows)=>{
         if(err){
             console.log(err)
         }else {
@@ -44,7 +60,7 @@ app.post('/mywork/',(req,res)=>{
 
 app.patch('/mywork',(req,res)=>{
     var mywork = req.body
-   connection.query('UPDATA  mywork SET ? WHERE id='+mywork.id,[mywork],(err,rows)=>{
+   connection.query('UPDATA  tasks SET ? WHERE id='+mywork.id,[mywork],(err,rows)=>{
        if(err){
            console.log(err)
        }else {
@@ -57,13 +73,13 @@ app.patch('/mywork',(req,res)=>{
 
 app.put('/mywork',(req,res)=>{
     var mywork = req.body
-   connection.query('UPDATE  mywork SET ? WHERE id='+mywork.id,[mywork],(err,rows)=>{
+   connection.query('UPDATE  tasks SET ? WHERE id='+mywork.id,[mywork],(err,rows)=>{
        if(err){
            console.log(err)
        }else {
             if(rows.affectedRows==0){
                 var myworkData =[mywork.name,mywork.salary]
-                connection.query('INSERT INTO mywork(title,task) values(?)',[myworkData],(err,rows)=>{
+                connection.query('INSERT INTO tasks(title,task) values(?)',[myworkData],(err,rows)=>{
                     if(err){
                         console.log(err)
                     }else {
@@ -77,9 +93,6 @@ app.put('/mywork',(req,res)=>{
        }
    })
 })
-
-
-
 
 
 app.listen(3000,()=>console.log('Express server is running on port 3000'))
